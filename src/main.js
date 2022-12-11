febd10d17355e561cf2958ff70212631cf06d02a
Mario <mariovidovic97@gmail.com>
2022-12-11 14:26:14 +0100

    aos npm

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from './App.vue'
 import './styles/style.scss';
 import router from './router'
 import store from './store'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  mounted() {
+    AOS.init()
+  },
   render: function (h) { return h(App) }
 }).$mount('#app')
