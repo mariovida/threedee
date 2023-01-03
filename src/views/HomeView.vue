24cc22a02aaa755eb087b3ca424121e8eb8360a5
Mario <mariovidovic97@gmail.com>
2023-01-03 14:26:14 +0100

    display fixes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,14 +23,17 @@
           <p>Welcome to <b>threedee</b>, where we bring your visions to life with stunning visualizations that captivate and engage your audience. Our team of skilled 3D artists, designers, and animators specialize in creating high-quality 3D models, animations, and virtual reality experiences for a wide range of industries and applications.</p>
           <p>Whether you need architectural visualizations for real estate projects, product animations for marketing campaigns, or immersive VR experiences for training or entertainment, we have the expertise and tools to deliver exceptional results. We use the latest software and techniques to ensure our 3D creations are accurate, realistic, and eye-catching.</p>
           <p>At our agency, we are committed to exceeding our clients' expectations and delivering projects on time and within budget. Contact us today to discuss your 3D needs and let us help you bring your ideas to life.</p>
-          <router-link to="./contact">CONTACT US</router-link>
+          <router-link to="./contact"><i class="bi-envelope"></i>CONTACT US</router-link>
         </div>
       </div>
     </section>
 
      <section class="categories">
       <div class="wrapper">
-        <h3>Our latest projects</h3>
+        <div class="header-row">
+          <h3>Our latest projects</h3>
+          <router-link to="./products">SEE MORE</router-link>
+        </div>
         <div class="category-box">
           <div class="category-item category-item_1"></div>
           <div class="category-item category-item_2"></div>
